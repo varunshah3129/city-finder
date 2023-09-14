3a311b5a195065a9e65d3b26ce0cf55f94827806
Varun Shah <varun.shah@envestnet.com>
2023-09-13 21:24:48 -0400

    key update

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 
 const config = {
-    googleApiKey: 'AIzaSyDlJl961BwPoWHaBRVJl93sqN74Z9qvSwM&',
+    googleApiKey: 'AIzaSyDlJl961BwPoWHaBRVJl93sqN74Z9qvSwM',
     openWeatherApiKey: '0350c5a250881adf28da66c7dab23807',
     foursquareClientId: 'W4ZEAGZ0JR0IILRNBP41PNC0OTII3KZD1DMPUY0F2HAD50VB',
     foursquareClientSecret: 'AP0HPT1NUC1SBTKNILAD3FBNPA5ZRWGAE0B3HU4NVLPAYRGZ',
